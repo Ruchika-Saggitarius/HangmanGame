06dd4603fe0e1d40cec555cc98dc0a19ad09ac7e
Ruchika Ruchika <rc315087@dal.ca>
2024-05-02 00:37:42 -0300

    test: :test_tube: added test

---
+++ hangman/src/test/java/com/ruchika/hangman/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.ruchika.hangman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ruchika.hangman.model.Role;
+import com.ruchika.hangman.requests.RegisterUserRequest;
+import com.ruchika.hangman.services.IUserService;
+
+//In this file we are writing test cases for UserService.java using JUnit and Mockito
+@SpringBootTest
+public class UserServiceTest{
+
+    @Autowired
+    private IUserService userService;
+
+    @Test
+    public void testSaveUser(){
+        RegisterUserRequest registerUserRequest = new RegisterUserRequest("ruchika123", "Ruchika@gmail.com", "ruchika123", Role.USER);
+        userService.saveUser(registerUserRequest);
+
+    }
+
+    
+}
